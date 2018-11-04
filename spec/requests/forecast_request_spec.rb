98bca8e67b8d04110b1091b806e84e5bac33c4ae
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-11-04 16:38:54 -0700

    completes api_v1_forecast test

--- spec/requests/forecast_request_spec.rb
+++ spec/requests/forecast_request_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 describe 'Forecast API' do
   it 'sends weather details' do
     location = 'denver,co'
-    get "api/v1/forecast?location=#{location}"
+    get "/api/v1/forecast?location=#{location}"
 
     expect(response).to be_successful
+    forecast_data = JSON.parse(response.body)
+    expect(forecast_data).to have_key('currently')
   end
 end
-
