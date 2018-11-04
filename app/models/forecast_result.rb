85c96c996e96ded6b5cfb5eb7db511da01e5396f
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-11-04 13:21:59 -0700

    darksky_service retrieves weather data, test passing. Adds forecast.rb to wrap data in objects. Code is refactored.

--- app/models/forecast_result.rb
+++ app/models/forecast_result.rb
@@ -7,14 +7,18 @@ class ForecastResult
     data[:results].first[:geometry][:location]
   end
   
-  # def weather_forecast
-  #   data = darksky_service.get_forecast(coordinates)
-  #   Forecast.new(data)
-  # end
+  def weather_forecast
+    data = darksky_service.get_forecast(coordinates)
+    Forecast.new(data)
+  end
   
   private 
   
   def geocode_service 
     GeocodeService.new
+  end
+  
+  def darksky_service
+    DarkskyService.new
   end
 end
--- app/services/darksky_service.rb
+++ app/services/darksky_service.rb
@@ -1,18 +1,18 @@
 class DarkskyService
-# 
-#   def get_forecast(coordinates)
-#     get_json("/forecast/#{ENV['darksky_api_key']}/#{coordinates[:lat]},#{coordinates[:lng]}")
-#   end
-# 
-#   private 
-# 
-#   def conn 
-#     Faraday.new(:url => "https://api.darksky.net") do |faraday|
-#       faraday.adapter Faraday.default_adapter
-#     end
-#   end 
-# 
-#   def get_json(url)
-#     JSON.parse(conn.get(url).body, symbolize_names: true)
-#   end
+
+  def get_forecast(coordinates)
+    get_json("/forecast/#{ENV['darksky_api_key']}/#{coordinates[:lat]},#{coordinates[:lng]}")
+  end
+
+  private 
+
+  def conn 
+    Faraday.new(:url => "https://api.darksky.net") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end 
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
 end
