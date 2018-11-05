e64319b3e478b83db5d66e823154eb587b948a03
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-11-05 11:22:59 -0700

    converts time integer to date-time for current, hourly, and daily time values

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -7,29 +7,47 @@ class Forecast
     @summary     =  data[:currently][:summary]
     @icon        =  data[:currently][:icon]
     @temperature =  data[:currently][:temperature]
-    @time        =  data[:currently][:time]
+    time         =  data[:currently][:time]
+    @time        =  DateTime.strptime(time.to_s,'%s')
     @feel_slike  =  data[:currently][:apparentTemperature]
     @humidity    =  data[:currently][:humidity]
     @visibility  =  data[:currently][:visibility]
     @uv_index    =  data[:currently][:uvIndex]
     @hourly      =  []
-    hourly       =  data[:hourly][:data].map do |hour|
-                      hours_hash = {}
+                    
+                    data[:hourly][:data].map do |hour|
+                      hash = {}
                       hour.map do |key, value|
                         if key == :time || key == :icon || key == :temperature
-                          hours_hash[key] = value
+                          hash[key] = value
+                        end
+                      end
+                      hours_hash = hash.map do |key, value|
+                        if key == :time
+                          DateTime.strptime(value.to_s,'%s')
+                        else
+                          hash[key] = value
                         end
                       end
                       @hourly << hours_hash
                     end
+                    
     @daily       =  []
-    daily        =  data[:daily][:data].map do |day|
-                      days_hash = {}
+                  
+                    data[:daily][:data].map do |day|
+                      hash = {}
                       day.map do |key, value|
                         if key == :time || key == :summary || key == :icon || key == :precipProbability || key == :precipType || key == :temperatureHigh || key == :temperatureLow
-                          days_hash[key] = value
+                          hash[key] = value
                         end 
                       end 
+                      days_hash = hash.map do |key, value|
+                        if key == :time
+                          DateTime.strptime(value.to_s,'%s')
+                        else
+                          hash[key] = value
+                        end
+                      end
                       @daily << days_hash
                     end
   end  
