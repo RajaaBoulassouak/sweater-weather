6f96cd1a69935ab8b706848f85a632818e9189ad
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-11-04 21:31:24 -0700

    modifies test according to data changes in forecast.rb file

--- spec/models/forecast_facade_spec.rb
+++ spec/models/forecast_facade_spec.rb
@@ -17,7 +17,7 @@ describe ForecastFacade do
     it 'has weather forecast data' do 
       location = 'denver,co'
       forecast_facade = ForecastFacade.new
-      
+
       expect(forecast_facade.weather_forecast).to be_a(Forecast) 
     end
   end
