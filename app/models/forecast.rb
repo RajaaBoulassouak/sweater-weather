b9c448e2e055b9591fb8533476350191bebcd46d
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-11-05 17:26:15 -0700

    refactors forecast initialize method

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -7,40 +7,49 @@ class Forecast
     @summary     =  data[:currently][:summary]
     @icon        =  data[:currently][:icon]
     @temperature =  data[:currently][:temperature]
-    time         =  data[:currently][:time]
-    @time        =  DateTime.strptime(time.to_s,'%s')
+    @time        =  converted_time(data)
     @feel_slike  =  data[:currently][:apparentTemperature]
     @humidity    =  data[:currently][:humidity]
     @visibility  =  data[:currently][:visibility]
     @uv_index    =  data[:currently][:uvIndex]
-    @hourly      =  []
-
-                    data[:hourly][:data].map do |hour|
-                      hour_hash = {}
-                      hour.map do |key, value|
-                        if key == :icon || key == :temperature
-                          hour_hash[key] = value
-                        elsif
-                          key == :time
-                            hour_hash[key] = DateTime.strptime(value.to_s,'%s')
-                        end
-                      end
-                      @hourly << hour_hash
-                    end
-                    
-    @daily       =  []
-                  
-                    data[:daily][:data].map do |day|
-                      day_hash = {}
-                      day.map do |key, value|
-                        if key == :summary || key == :icon || key == :precipProbability || key == :precipType || key == :temperatureHigh || key == :temperatureLow
-                          day_hash[key] = value
-                        elsif
-                          key == :time
-                            day_hash[key] = DateTime.strptime(value.to_s,'%s')
-                        end 
-                      end 
-                      @daily << day_hash
-                    end
+    @hourly      =  hourly_forecast(data)
+    @daily       =  daily_forecast(data)            
   end 
+  
+  def converted_time(data)
+    time = data[:currently][:time]
+    DateTime.strptime(time.to_s,'%s')
+  end
+  
+  def hourly_forecast(data) 
+    hours_array = []
+    data[:hourly][:data].map do |hour|
+      hour_hash = {}
+      hour.map do |key, value|
+        if key == :icon || key == :temperature
+          hour_hash[key] = value
+        elsif
+          key == :time
+            hour_hash[key] = DateTime.strptime(value.to_s,'%s')
+        end
+      end
+      hours_array << hour_hash
+    end
+  end
+  
+  def daily_forecast(data)
+    days_array = []
+    data[:daily][:data].map do |day|
+      day_hash = {}
+      day.map do |key, value|
+        if key == :summary || key == :icon || key == :precipProbability || key == :precipType || key == :temperatureHigh || key == :temperatureLow
+          day_hash[key] = value
+        elsif
+          key == :time
+            day_hash[key] = DateTime.strptime(value.to_s,'%s')
+        end 
+      end 
+      days_array << day_hash
+    end
+  end
 end 
